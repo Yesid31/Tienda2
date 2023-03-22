ffa5eed1113edd0af1322df6a2badfe8ee4e40b8
Yeison Garzon T <toxiicsa@hotmail.com>
2023-03-22 08:52:33 -0500

    completo

--- src/controllers/controladorCompra.js
+++ src/controllers/controladorCompra.js
@@ -1,36 +1,62 @@
 //Llamo a la memoria para acceder a los datos guardados:
-let infoProducto= JSON.parse(localStorage.getItem("producto"))
-console.log(infoProducto)
-let foto=document.getElementById("foto")
-foto.src=infoProducto.foto
+import { EjecutarApiTasa } from "../helpers/ampliarinfo.js";
+import { formateador } from "../helpers/pintarProductos.js";
+let infoProducto = JSON.parse(localStorage.getItem("producto"));
+console.log(infoProducto);
+let foto = document.getElementById("foto");
+foto.src = infoProducto.foto;
 
-let nombre=document.getElementById("nombre")
-nombre.textContent=infoProducto.nombre
+let nombre = document.getElementById("nombre");
+nombre.textContent = infoProducto.nombre;
 
-let precio=document.getElementById("precio")
-precio.textContent=infoProducto.precio
+let precio = document.getElementById("precio");
+precio.textContent = infoProducto.precio;
 
-let infocarrito=JSON.parse(localStorage.getItem("carrito"))
-let carrito
-let pildora=document.getElementById("pildora") 
-if(infocarrito!=null){
-  carrito=infocarrito
-  pildora.textContent=carrito.length
+let infocarrito = JSON.parse(localStorage.getItem("carrito"));
+let carrito;
+let pildora = document.getElementById("pildora");
+if (infocarrito != null) {
+  carrito = infocarrito;
+  pildora.textContent = carrito.length;
+} else {
+  carrito = [];
 }
-else{
-     carrito=[]
-}
+
+let botonAgregarProducto = document.getElementById("btnagregarcarrito");
+
+botonAgregarProducto.addEventListener("click", function () {
+  carrito.push(infoProducto);
+  pildora.textContent = carrito.length;
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+});
+
+let botonConvertir = document.getElementById("conversor");
+botonConvertir.addEventListener("click", async () => await EjecutarApiTasa());
+
+// evitando problemas con el input de cantidad
+let cantidad = document.getElementById("cantidad");
+cantidad.addEventListener("keydown", (tecla) => {
+  let teclasProhibidas = ["e", "-", "+", "*", "/"];
+  if (teclasProhibidas.includes(tecla.key)) {
+    tecla.preventDefault();
+  }
+});
+
+//Subtotal
+let Subtotal = document.getElementById("SpanSubtotal");
+cantidad.addEventListener("input", function () {
+ 
+  let CalcularSubtotal = Number(cantidad.value) * Number(infoProducto.precio);
+  Subtotal.textContent = CalcularSubtotal;
+});
+//Limpiar Carrito
+let botonEliminarProducto=document.getElementById("btneliminar")
+
+  botonEliminarProducto.addEventListener("click",function(){
+    carrito.splice(infoProducto)
+    pildora.textContent =carrito.length 
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+
+  })
 
 
-
-
-
-
-let botonAgregarProducto=document.getElementById("btnagregarcarrito")
-
-botonAgregarProducto.addEventListener("click",function(){
-   
-    carrito.push(infoProducto)
-   pildora.textContent=carrito.length
-   localStorage.setItem("carrito",JSON.stringify(carrito))
-})
--- src/helpers/pintarProductos.js
+++ src/helpers/pintarProductos.js
@@ -1,49 +1,79 @@
-export function pintarProductos(productos){
+export const formateador = new Intl.NumberFormat('en-US', {
+    
+})
 
+export function pintarProductos(productos){
+    
     productos.forEach(function(producto){
+
         //creando columnas para alojar los productos
+   
         let columna=document.createElement("div")
         columna.classList.add("col")
-    
-        // Crear una Tarjeta para almacenar info del producto
-    
+   
+        //voy a crear una tarjeta oara almacenar la info del producto
+   
         let tarjeta=document.createElement("div")
         tarjeta.classList.add("card","h-100","shadow")
-    
-        //crear una imagen 
+   
+        //crear una imagen
+   
         let imagen=document.createElement("img")
         imagen.classList.add("img-fluid","w-100")
         imagen.src=producto.fotos[0]
-    
-        // Crear Un Titulo
+   
+        //Crear un titulo
+   
         let nombre=document.createElement("h3")
         nombre.classList.add("text-center")
         nombre.textContent=producto.nombre
-        // Crear La Descripcion
-        let descripcion=document.createElement("h6")
+
+        //crear la descripcion
+
+        let descripcion=document.createElement("p")
         descripcion.classList.add("content-right","d-none")
         descripcion.textContent=producto.descripcion
-        // Crear Precio 
-        let precio=document.createElement("h5")
-        precio.classList.add("text-center","text-success","fw-blod")
-        precio.textContent="USD "+producto.precio
+   
+        //crear un precio
+   
+        //let precio=document.createElement("h5")
+        //precio.classList.add("text-center","text-success","fw-bold")
+        //precio.textContent="COP"+producto.precio
+
+        // crear un precio
+        let precioDisplay = document.createElement("h5")
+        precioDisplay.classList.add("text-center", "text-rojo", "fw-bold", "mb-3")
+        precioDisplay.textContent = (producto.precio)
+        
     
-        // Detectando evento
+        let precioOculto = document.createElement("h1")
+        precioOculto.classList.add("d-none")
+        precioOculto.textContent = producto.precio
+
+        //Detectando evento
+
         tarjeta.addEventListener("mouseover",function(){
+            
             imagen.src=producto.fotos[1]
-           
+
         })
+
         tarjeta.addEventListener("mouseleave",function(){
+            
             imagen.src=producto.fotos[0]
-           
+
         })
+   
         //padres e hijos
+   
         tarjeta.appendChild(nombre)
         tarjeta.appendChild(imagen)
         tarjeta.appendChild(descripcion)
-        tarjeta.appendChild(precio)
+        tarjeta.appendChild(precioDisplay)
+        tarjeta.appendChild(precioOculto)
+        //tarjeta.appendChild(precio)
         columna.appendChild(tarjeta)
         fila.appendChild(columna)
-    
-    })
+   
+   })
 }
